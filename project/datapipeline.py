2be631ef22b1c9b1729a4b7dcee15c2fdfce2ae8
alexi0013 <167550586+alexi0013@users.noreply.github.com>
2024-05-23 13:50:09 +0200

    Update pipeline.py

--- project/datapipeline.py
+++ project/datapipeline.py
@@ -1,1 +1,10 @@
+# Python standard libraries
+import json
+import os
+import zipfile
+import requests
+import numpy as np
+import pandas as pd
 
+# Python kaggle data
+from kaggle.api.kaggle_api_extended import KaggleApi
